4ebd6c1e9eb7798620db55f4f5ad16f1587e92a4
Didenko Andrey <mozggg1@gmail.com>
2023-06-08 14:30:53 +0700

    Fixes based on Andrey M comments

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # Requirement Tracker
 
 All system configurations, rules, and support services outside the initial deployment scope of work will be documented and approved using the Requirement Tracker in OneVizion.
+
 Requirements may be added by either Client or OneVizion.
+
 Requirements will be updated continuously and reviewed weekly during the Change Management meeting.
+
 Each requirement will be categorized as In-Scope or Out of Scope.
+
 In-Scope items will be worked using best efforts to meet launch targets and expectations.
+
 Out of Scope items will be reviewed by OneVizion. More information may be needed to complete the estimate, or the request may not be possible.
+
 Once accepted, OneVizion will provide a level of effort to be drawn against the managed services budget, and an estimated delivery date for testing.
+
 Depending on the complexity of the request and any subsequent changes received from Client may increase the effort and time to deliver the requirement.
 
+Additional documentation you can find in documentation folder on https://github.com/ov-integrations/requirements-trackor or https://trackor.onevizion.com/form/ConfigAppForm.do?id=100101237244&ttid=100094267
--- installation/140.Rules-Activation.sql
+++ installation/140.Rules-Activation.sql
@@ -1,45 +1,31 @@
-begin
-    for rec in (
-        select rule_id, rule, sql_text
+declare
+    cursor cur_rules_by_comp_package(p_comp_package in components_package.name%type) is
+        select rule.rule_id, rule.rule, rule.sql_text 
           from rule
-         where rule in (
-             'VPRT Set Requirement Created Date', 
-             'VPRT Update Requirements Comment History', 
-             'VPRT Copy Comments to Chat', 
-             'VPRT Set Requirement Created By', 
-             'VPRT Copy Chat to Comments Field', 
-             'VPRT Create OV Change Request'
-        )
-    ) loop
-        dbms_output.put_line('Enabling rule [' ||  rec.rule || ']' || pkg_str.c_lb);
-        pkg_ruleator.compile_rule_plsql_block_and_raise(rec.sql_text);
-        update rule set is_enabled = 1 where rule_id = rec.rule_id;
-        commit;
+          join comp_pkg_object_xref cpx on cpx.object_id = rule.rule_id
+          join components_package cp on cp.components_package_id = cpx.components_package_id
+         where cpx.component_id = pkg_audit_comp.c_rule_component_id
+           and cp.name = p_comp_package;
+           
+    cursor cur_autom_by_comp_package(p_comp_package in components_package.name%type) is
+        select autom.autom_id, autom.autom_name
+          from autom
+          join comp_pkg_object_xref cpx on cpx.object_id = autom.autom_id
+          join components_package cp on cp.components_package_id = cpx.components_package_id
+         where cpx.component_id = pkg_audit_comp.c_autom_component_id
+           and cp.name = p_comp_package;
+begin
+    for rec_rule in cur_rules_by_comp_package('VP - Requirements Trackor') loop
+        dbms_output.put_line('Enabling rule [' ||  rec_rule.rule || ']' || pkg_str.c_lb);
+        pkg_ruleator.compile_rule_plsql_block_and_raise(rec_rule.sql_text);
+
+        update rule set is_enabled = 1 where rule_id = rec_rule.rule_id;
     end loop;
-    dbms_output.put_line('Rules Enabled' || pkg_str.c_lb);
-    update autom
-       set enabled = 1
-     where autom_name in (
-     'VPRT REQ_PROD_RLSD_DATE', 
-     'VPRT REQ_RELEASE_TO_ONEVIZION_DATE', 
-     'VPRT REQ_REQUIREMENT_ACCEPTED_DATE',
-     'VPRT REQ_RELEASE_TO_ONEVIZION new', 
-     'VPRT REQ_ADDITIONAL_DETAIL_NEEDED', 
-     'VPRT REQ_READY_TO_TEST',
-     'VPRT REQ_ADDITIONAL_DETAIL_NEEDED1', 
-     'VPRT REQ_ESTIMATE_ACCEPTED_DATE', 
-     'VPRT REQ_REQUIREMENT_ACCEPTED',
-     'VPRT REQ_READY_TO_TEST_DATE', 
-     'VPRT REQ_ESTIMATE_TO_CUSTOMER_DATE', 
-     'VPRT REQ_RELEASE_TO_ONEVIZION old',
-     'VPRT REC_ACTUAL_COMPLETE', 
-     'VPRT I_ISTATUS', 
-     'VPRT REQ_REQUIREMENT_STATUS_CHANGE_',
-     'VPRT REQ_ADDITIONAL_DETAIL_REQ_DATE', 
-     'VPRT REQ_REQ_ACCEPTED_UAT_DATE', 
-     'VPRT REQ_PRODUCTION_RELEASED',
-     'VPRT REQ_READY_TO_TEST 2',
-     'VPRT I_OWNER'
-    );
-    dbms_output.put_line('Automations Enabled');
+    
+    for rec_autom in cur_autom_by_comp_package('VP - Requirements Trackor') loop
+        dbms_output.put_line('Enabling automation [' ||  rec_autom.autom_name || ']' || pkg_str.c_lb);
+
+        update autom set enabled = 1 where autom_id = rec_autom.autom_id;
+    end loop;
+    commit;
 end;
